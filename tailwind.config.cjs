749ecf110c510866abf1dec15f4142a8e826314b
tascintra <tiago.slv@hotmail.com>
2022-10-13 18:22:55 -0300

    tailwind colors restructured as objects

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,15 +17,19 @@ module.exports = {
       'black': '000',
       'white': 'FFF',
 
-      'gray-500': '#121214',
-      'gray-400': '#202024',
-      'gray-300': '#7C7C8A',
-      'gray-200': '#C4C4CC',
-      'gray-100': '#E1E1E6',
+      gray: {
+        500: '#121214',
+        400: '#202024',
+        300: '#7C7C8A',
+        200: '#C4C4CC',
+        100: '#E1E1E6',
+      },
 
-      'cyan-100': '#81D8F7',
-      'cyan-200': '#92DEF7',
-      'cyan-300': '#A7E8FA',
+      cyan: {
+        100: '#81D8F7',
+        200: '#92DEF7',
+        300: '#A7E8FA',
+      }
     },
     extend: {
       fontFamily: {
