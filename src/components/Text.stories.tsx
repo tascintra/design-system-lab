a64c90c26f303e27202aff9ae3f2329d34d18a15
tascintra <tiago.slv@hotmail.com>
2022-10-13 15:42:46 -0300

    diferent text component style created

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,25 @@
-import { Text } from './Text'
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from './Text';
 
 export default {
   title: 'Components/Text',
   component: Text,
-}
+  args: {
+    children: 'Lorem ipsum',
+    size: 'lg',
+  },
+} as Meta<TextProps>;
 
-export const Default = {}
+export const Default: StoryObj<TextProps> = {};
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm',
+  },
+};
+
+export const Large: StoryObj<TextProps> = {
+  args:{
+    size: 'lg'
+  }
+};
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,9 +1,21 @@
-interface TextProps {
+
+import { clsx } from 'clsx';
+
+export interface TextProps {
   size?: 'sm' | 'md' | 'lg';
+  children: string;
 }
 
-export function Text(props: TextProps) {
+export function Text({ size = 'md', children }: TextProps) {
   return (
-    <span className="text-gray-100 font-sans">Text</span>
-  )
+    <span
+      className={clsx('text-gray-100 font-sans', {
+        'text-xs': size === 'sm',
+        'text-sm': size === 'md',
+        'text-md': size === 'lg',
+      })}
+    >
+      {children}
+    </span>
+  );
 }
