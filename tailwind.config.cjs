72f012ce17971484c345f8b6c0f7d566bf83355d
tascintra <tiago.slv@hotmail.com>
2022-10-13 13:41:37 -0300

    font size difinition

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,14 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
-    
-
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
 
     colors: {
       'gray-500': '#121214',
