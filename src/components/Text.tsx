51564a108e64ad21c9d6cdf7ad18eb4d64a7968f
tascintra <tiago.slv@hotmail.com>
2022-10-13 15:46:09 -0300

    created const to give users choice to get a tag

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,14 +1,16 @@
-
+import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
 
 export interface TextProps {
   size?: 'sm' | 'md' | 'lg';
   children: string;
+  asChild?: boolean;
 }
 
-export function Text({ size = 'md', children }: TextProps) {
+export function Text({ size = 'md', children, asChild }: TextProps) {
+  const Comp = asChild ? Slot : 'span'
   return (
-    <span
+    <Comp
       className={clsx('text-gray-100 font-sans', {
         'text-xs': size === 'sm',
         'text-sm': size === 'md',
@@ -16,6 +18,6 @@ export function Text({ size = 'md', children }: TextProps) {
       })}
     >
       {children}
-    </span>
+    </Comp>
   );
 }
