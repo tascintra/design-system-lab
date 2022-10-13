a077e1b919a7207a66f149d548562900b709095a
tascintra <tiago.slv@hotmail.com>
2022-10-13 18:25:10 -0300

    TextInput style created

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -9,8 +9,8 @@ export function TextInput({}: TextInputProps) {
   return (
     <input
       className={clsx(
-        '',
-    )}
+        'py-4 px-3 rounded bg-gray-400 w-full text-gray-100 text-xs placeholder:text-gray-300',
+      )}
     />
   );
 }
