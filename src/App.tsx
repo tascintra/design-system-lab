d9633975c43a2be329d8543ac96d08c5f3af02ef
tascintra <tiago.slv@hotmail.com>
2022-10-14 19:54:02 -0300

    fix: fixed button height and added it to app

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ export function App() {
             <Text size='sm' className='text-gray-200'>Lembrar de mim por 30 dias</Text>
           </label>
 
-          <Button type="submit">Entrar na plataforma</Button>
+          <Button type="submit" className='mt-4'>Entrar na plataforma</Button>
         </form>
       </div>
     </>
