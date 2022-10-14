56ce878532d355ddd2283e24ed98963ff800e7be
tascintra <tiago.slv@hotmail.com>
2022-10-14 19:11:22 -0300

    header area done and added form with a text for email and the email textinput

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Envelope } from 'phosphor-react';
 import { Heading } from './components/Heading';
 import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
 import './styles/global.css';
 import { Logo } from './styles/Logo';
 
@@ -7,13 +9,25 @@ export function App() {
   return (
     <>
       <div className="w-screen h-screen bg-gray-500 flex items-center justify-center text-gray-100">
-        <Logo />
+        <header className='flex flex-col items-center'>
+          <Logo />
+          <Heading size='lg' className='mt-4'>
+            Ignite Lab
+          </Heading>
+          <Text size='lg' className='text-gray-300 mt-1'>Faça login e comece a usar!</Text>
+        </header>
 
-        <Heading size='lg'>
-          Ignite Lab
-        </Heading>
-
-        <Text size='lg' className='text-gray-300'>Faça login e comece a usar!</Text>
+        <form className='flex flex-col items-stretch'>
+          <label htmlFor="email">
+            <Text className='font-semibold'>Endereço de e-mail</Text>
+            <TextInput.Root>
+              <TextInput.Icon>
+                <Envelope />
+              </TextInput.Icon>
+              <TextInput.Input placeholder='Digite seu e-mail'/>
+            </TextInput.Root>
+          </label>
+        </form>
       </div>
     </>
   )
