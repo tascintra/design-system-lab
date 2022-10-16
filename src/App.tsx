87c5e433295e96be3ad6bab7a7ab3ad82dc2d4ba
tascintra <tiago.slv@hotmail.com>
2022-10-16 20:13:12 -0300

    created file for storybook signin documentation and removed imports from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { Envelope, Lock } from "phosphor-react";
-import { Button } from "./components/Button";
-import { Checkbox } from "./components/Checkbox";
-import { Heading } from "./components/Heading";
 import { SignIn } from "./components/pages/SignIn";
-import { Text } from "./components/Text";
-import { TextInput } from "./components/TextInput";
 import "./styles/global.css";
-import { Logo } from "./styles/Logo";
 
 export function App() {
   return (
