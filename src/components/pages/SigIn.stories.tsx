cb6c1fe5a8d7d2b30d5d4042e77da0411ceb8293
tascintra <tiago.slv@hotmail.com>
2022-10-16 21:27:18 -0300

    sotybook testing libs installation

--- src/components/pages/SigIn.stories.tsx
+++ src/components/pages/SigIn.stories.tsx
@@ -1,5 +1,7 @@
-import { Meta, StoryObj } from '@storybook/react';
-import { SignIn } from './SignIn';
+import { Meta, StoryObj } from '@storybook/react'
+import { within, userEvent } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
+import { SignIn } from './SignIn'
 
 export default {
   title: 'Pages/Sign in',
@@ -8,4 +10,6 @@ export default {
   argTypes: {}
 } as Meta
 
-export const Default: StoryObj = {};
+export const Default: StoryObj = {
+  
+};
