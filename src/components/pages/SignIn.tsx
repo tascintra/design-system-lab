b6b18c57a42bf4e5df6408d887a419fe34b2e5c6
tascintra <tiago.slv@hotmail.com>
2022-10-18 21:36:27 -0300

    fix: checkbox not being displayed fixed

--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Checkbox } from '@radix-ui/react-checkbox';
+import { Checkbox } from '../Checkbox';
 import { Envelope, Lock } from 'phosphor-react';
 import axios from 'axios';
 import { Logo } from '../../styles/Logo';
